5c15c23bf5579806ea56b7df2e0d27925dfdd01b
Sakib <najmuskg@gmail.com>
2024-03-02 19:13:01 +0600

    all files modified

--- markup/index.php
+++ markup/index.php
@@ -2,31 +2,64 @@
 <html lang="en">
 
 <head>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
+    <!-- Title  -->
     <title>Home | Misato Masjid</title>
 
+    <!-- Core Style Css -->
     <link rel='stylesheet' id='theme-style-css' href='/wp-content/themes/masjid/_/css/screen.min.css' type='text/css' media='all' />
+
 </head>
 
 <body>
 
+    <!-- ==================== Start Loading ==================== -->
+
+    <div class="loader-wrap">
+        <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+            <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+        </svg>
+
+        <div class="loader-wrap-heading">
+            <div class="load-text">
+                <span>L</span>
+                <span>o</span>
+                <span>a</span>
+                <span>d</span>
+                <span>i</span>
+                <span>n</span>
+                <span>g</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- ==================== End Loading ==================== -->
+
+    <!-- ==================== Start main--wrapper ==================== -->
+
     <div class="main--wrapper">
 
-        <section class="header--wrapper">
+        <!-- ==================== Start header--wrapper ==================== -->
 
+        <section class="header--wrapper">
             <header class="header">
                 <div class="holder">
+
                     <div class="header__logo">
-                        <h1 title="Misato Masjid"><a href="index.php"><img src="/wp-content/themes/masjid/_/images/logo.png" alt="Misato Masjid">misato <span>masjid</span></a></h1>
+                        <h1 title="Misato Masjid"><a href="index.html"><img src="/wp-content/themes/masjid/_/images/logo.png" alt="Misato Masjid">misato <span>masjid</span></a></h1>
                     </div>
 
                     <div class="header__right">
-                        <div class="donate">
+
+                        <div class="donate pulse">
                             <a href="#" class="btn">Donate Us</a>
                         </div>
+
                         <div class="switch">
                             <input id="language-toggle" class="check-toggle check-toggle-round-flat" type="checkbox">
                             <label for="language-toggle"></label>
@@ -36,17 +69,25 @@
 
                     </div>
                 </div>
-            </header><!-- /.header -->
+            </header><!-- Header -->
 
 
             <div class="banner--wrapper">
                 <div class="jarallax" data-speed="0.5" style="background-image: url('/wp-content/themes/masjid/_/images/masjid.jpg');"></div>
-            </div><!-- /.banner--wrapper -->
+            </div><!-- banner--wrapper -->
 
-        </section><!-- /.header--wrapper -->
+        </section>
+
+        <!-- ==================== End header--wrapper ==================== -->
 
         <div class="holder container">
+
+            <!-- ==================== Start Article ==================== -->
+
             <article>
+
+                <!-- ==================== Start announcements--wrapper ==================== -->
+
                 <section class="announcements--wrapper">
                     <h2>Announcements</h2>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
@@ -63,7 +104,8 @@
                                 <div class="btn">Read More</div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- card -->
+
                     <div class="card bg-white">
                         <img src="/wp-content/themes/masjid/_/images/eId.png" alt="">
                         <div class="card-info">
@@ -75,25 +117,35 @@
                                 <div class="btn">Read More</div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- card -->
 
-                </section>
+                    <!-- ==================== End announcements--wrapper ==================== -->
+
             </article>
 
+            <!-- ==================== End Article ==================== -->
+
+            <!-- ==================== Start Aside ==================== -->
+
             <aside>
+
                 <div class="timesCard">
+
                     <h4>Prayer Times</h4>
+
                     <div class="bg-white">
                         <div class="tab-wrapper">
+
                             <ul class="tabs">
                                 <li class="tab-link"><a href="#tab-1">Prayer Times</a> </li>
                                 <li class="tab-link"><a href="#tab-2">Jum'ah Prayer</a> </li>
                             </ul>
-                        </div>
+
+                        </div><!-- tab-wrapper -->
+
                         <p>Ithnin, 25 Muharram 1444 August 23, 2022 (Tue)</p>
 
                         <div class="content-wrapper">
-
                             <div id="tab-1" class="tab-content">
                                 <table class="rwd-table">
                                     <tbody>
@@ -101,170 +153,226 @@
                                             <td>
                                                 Fazr
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 03:33
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Sunrise
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 05:05
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Zuhr
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 11:44
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Asr
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 15:32
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Sunset
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 18:32
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Magrib
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 18:28
                                             </td>
+
                                         </tr>
+
                                         <tr>
                                             <td>
                                                 Isha
                                             </td>
+
                                             <td>
                                                 <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                             </td>
+
                                             <td>
                                                 19:58
                                             </td>
+
                                         </tr>
+
                                     </tbody>
-                                </table>
-                            </div>
+
+                                </table><!-- rwd-table -->
+
+                            </div><!-- tab-content -->
 
                             <div id="tab-2" class="tab-content">
                                 <h3>Sakib</h3>
-                            </div>
+                            </div><!-- tab-content -->
 
                         </div>
                     </div>
-                </div>
+                </div><!-- TimeCard -->
+
+
                 <div class="timesCard">
+
                     <h4>Iqamah Times</h4>
+
                     <div class="bg-white">
-
                         <div class="content-wrapper">
-
                             <table class="rwd-table">
                                 <tbody>
                                     <tr>
                                         <td>
                                             Fazr
                                         </td>
+
                                         <td>
                                             <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                         </td>
+
                                         <td>
                                             04:25
                                         </td>
+
                                     </tr>
 
                                     <tr>
                                         <td>
                                             Zuhr
                                         </td>
+
                                         <td>
                                             <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                         </td>
+
                                         <td>
                                             12:30
                                         </td>
+
                                     </tr>
+
                                     <tr>
                                         <td>
                                             Asr
                                         </td>
+
                                         <td>
                                             <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                         </td>
+
                                         <td>
                                             16:00
                                         </td>
+
                                     </tr>
 
                                     <tr>
                                         <td>
                                             Magrib
                                         </td>
+
                                         <td>
                                             <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                         </td>
+
                                         <td>
                                             Soon <br> After <br> Azan
                                         </td>
+
                                     </tr>
+
                                     <tr>
                                         <td>
                                             Isha
                                         </td>
+
                                         <td>
                                             <img src="/wp-content/themes/masjid/_/images/miner_green.png" alt="Masjid">
                                         </td>
+
                                         <td>
                                             20:00
                                         </td>
+
                                     </tr>
                                 </tbody>
-                            </table>
+                            </table><!-- rwd-table -->
 
                             <div id="tab-2" class="tab-content">
+
                                 <h3>Sakib</h3>
-                            </div>
+
+                            </div><!-- tab-content -->
 
                         </div>
                     </div>
-                </div>
+                </div><!-- TimeCard -->
             </aside>
 
         </div>
 
     </div>
+
+    <!-- ==================== End main--wrapper ==================== -->
+
+
+    <!-- ==================== Start footer ==================== -->
 
     <footer class="footer">
         <div class="footer--top">
@@ -336,12 +444,20 @@
         </div>
 
     </footer>
-    <!-- ============= End of /footer =============== -->
+
+    <!-- ==================== End footer ==================== -->
 
 
+    <!-- jQuery -->
     <script type='text/javascript' src='/wp-includes/js/jquery/jquery.min.js?ver=3.6.0'></script>
+
+    <!-- plugins -->
     <script type='text/javascript' src='/wp-content/themes/masjid/_/js/jarallax.min.js'></script>
+    <script type='text/javascript' src='/wp-content/themes/masjid/_/js/gasp.min.js'></script>
+
+    <!-- custom scripts -->
     <script type='text/javascript' src='/wp-content/themes/masjid/_/js/functions.js'></script>
+
 
 </body>
 
--- wp-content/themes/masjid/_/js/functions.js
+++ wp-content/themes/masjid/_/js/functions.js
@@ -2,17 +2,6 @@
 var $ = jQuery.noConflict();
 
 $(function () {
-  // $('.banner--wrapper .scroll--down__arrow').on('click', function (e) {
-  //   // var href = $(this).attr('href');
-  //   e.preventDefault();
-
-  //   $('html, body').animate({
-  //     scrollTop: $('section:nth-child(2)').offset().top
-  //   }, '5000');
-  // });
-
-  /* Show Header when scroll up */
-  /* ------------------------------------------- */
   var lastScrollTop = 0;
   var $header = $("header");
   var headerHeight = $header.outerHeight();
@@ -38,7 +27,6 @@ $(function () {
   });
 
   /* Main Nav */
-  /* ------------------------------------------- */
   if ($(window).width() < 1080) {
     $(".main--menu .menu-item-has-children span").on("click", function (event) {
       event.preventDefault();
@@ -59,7 +47,7 @@ $(function () {
   }
 
   /* Sticky Menu */
-  /* ------------------------------------------- */
+
   $(".toggle-menu").on("click", function () {
     $("body").toggleClass("menu-extended");
     $("header").toggleClass("nav-open");
@@ -70,7 +58,7 @@ $(function () {
   // End of Sticky Menu
 
   /* Filter drop down */
-  /* ------------------------------------------- */
+
   $(".filter-title").click(function () {
     $(this).data("clicked", true);
     var $this = $(this);
@@ -79,40 +67,9 @@ $(function () {
     $(this).next().toggleClass("filter-active");
   });
 
-  /* Themes Select */
-  /* ------------------------------------------- */
-
-  // Retrieve the stored checkbox state from localStorage
-  var isChecked = localStorage.getItem("checkboxState");
-
-  if (isChecked === "true") {
-    $(".themeSelect input[type=checkbox]").prop("checked", true);
-    $("body").addClass("themeLight");
-  }
-
-  $(".themeSelect input[type=checkbox]").change(function () {
-    if (this.checked) {
-      $("body").addClass("themeLight");
-      // Store the checkbox state as checked in localStorage
-      localStorage.setItem("checkboxState", true);
-    } else {
-      $("body").removeClass("themeLight");
-      // Store the checkbox state as unchecked in localStorage
-      localStorage.setItem("checkboxState", false);
-    }
-  });
-
-  /* Themes Select */
-  /* ------------------------------------------- */
-
-  $(".cloak").click(function () {
-    $(this).parent().removeClass("filter-active");
-  });
   // End of Filter drop down
 
   /* Footer Menu Slide Mobile */
-  /* ------------------------------------------- */
-
   if ($(window).width() < 768) {
     $(".filters--title").on("click", function () {
       $(this).toggleClass("filter-open");
@@ -132,7 +89,6 @@ $(function () {
   // End of Footer Menu Slide Mobile
 
   /* Footer Menu Slide Mobile */
-  /* ------------------------------------------- */
   $('.scroll--nav a[href^="#"]').on("click", function (event) {
     var target = $(this.getAttribute("href"));
     if (target.length) {
@@ -150,20 +106,15 @@ $(function () {
   // End of Footer Menu Slide Mobile
 
   /* Language Selector */
-  /* ------------------------------------------- */
-
   $(".nav-toggle").on("click", function () {
     $(this).toggleClass("footer-menu-expand");
     $(".footer--nav").slideToggle();
   });
-
   // End of Language Selector
 
   /* Parallax Effect on scroll */
   /* ------------------------------------------- */
   $(".jarallax").jarallax();
-
-
 
   /* Custom Tabs */
   /* ------------------------------------------- */
@@ -180,17 +131,6 @@ $(function () {
       e.preventDefault();
     }
   });
-
-
-  // $('.tab-link').click( function() {
-
-  //   var tabID = $(this).attr('data-tab');
-
-  //   $(this).addClass('active').siblings().removeClass('active');
-
-  //   $('#tab-'+tabID).addClass('active').siblings().removeClass('active');
-  // });
-
   // End of Tabs
 
   var inf_scrll = jQuery(".next");
@@ -232,14 +172,7 @@ $(function () {
 
 /* window load class helper */
 /* ------------------------------------------- */
-$(window).on("load", function () {
-  // setTimeout(function () {
-  $("html").removeClass("loading").addClass("loaded");
-  // }, 10000);
-});
-
 /* manually doing in-view stuff */
-/* ------------------------------------------- */
 
 const scrollers = document.querySelectorAll("[data-scroll]");
 
@@ -254,3 +187,120 @@ function check(entries) {
 
 const observer = new IntersectionObserver(check);
 scrollers.forEach((scroller) => observer.observe(scroller));
+
+$(function () {
+  "use strict";
+
+  var wind = $(window);
+
+  /* =============================================================================
+    -------------------------------  Preloader svg   -------------------------------
+    ============================================================================= */
+
+  const svg = document.getElementById("svg");
+  const tl = gsap.timeline();
+  const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+  const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+  tl.to(".loader-wrap-heading .load-text , .loader-wrap-heading .cont", {
+    delay: 1.5,
+    y: -100,
+    opacity: 0,
+  });
+  tl.to(svg, {
+    duration: 0.5,
+    attr: { d: curve },
+    ease: "power2.easeIn",
+  }).to(svg, {
+    duration: 0.5,
+    attr: { d: flat },
+    ease: "power2.easeOut",
+  });
+  tl.to(".loader-wrap", {
+    y: -1500,
+  });
+  tl.to(".loader-wrap", {
+    zIndex: -1,
+    display: "none",
+  });
+  tl.from(
+    "header",
+    {
+      y: 200,
+    },
+    "-=1.5"
+  );
+  tl.from(
+    "header .container",
+    {
+      y: 40,
+      opacity: 0,
+      delay: 0.3,
+    },
+    "-=1.5"
+  );
+});
+
+$(window).on("load", function () {
+  /* =============================================================================
+  ---------------------------------  Preloader  ----------------------------------
+  ============================================================================= */
+
+  var body = $("body");
+  body.addClass("loaded");
+  setTimeout(function () {
+    body.removeClass("loaded");
+  }, 1500);
+});
+
+/* =============================================================================
+  -----------------------------  Button scroll up   ------------------------------
+  ============================================================================= */
+
+$(document).ready(function () {
+  "use strict";
+
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 150;
+  var duration = 550;
+  jQuery(window).on("scroll", function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".progress-wrap").addClass("active-progress");
+    } else {
+      jQuery(".progress-wrap").removeClass("active-progress");
+    }
+  });
+  jQuery(".progress-wrap").on("click", function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, duration);
+    return false;
+  });
+});
+
+/* =============================================================================
+  -----------------------------  Progress   ------------------------------
+  ============================================================================= */
+
+$(document).scroll(function () {
+  var scrollTop = $(document).scrollTop();
+  var scrollHeight = $(document).height();
+  var windowHeight = $(window).height();
+  var scrollBottom = scrollHeight - windowHeight;
+  var scrollPercent = (scrollTop / scrollBottom) * 100 + "%";
+  $("#progress").css("--scroll", scrollPercent);
+});
